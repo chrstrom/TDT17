5e6f3bea4292b6a15b909da9aeba64ec346d0677
Christopher Strøm <detoxte@gmail.com>
2022-11-22 19:54:50 +0100

    Make data loading package
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- mini-project/data_loading/config.py
+++ mini-project/data_loading/config.py
@@ -3,10 +3,10 @@ CLASSES = ['D00','D10','D20','D40']
 SUPERCAT = ["crack", "crack", "crack", "pothole"]
 
 
-IMAGE_DIR = "./Norway/train/images"
-ANN_DIR = "./Norway/train/annotations/xmls"
-JSON_DIR = "./NorwayJSON"
-COCO_DIR = "./NorwayCOCO"
+IMAGE_DIR = "../Norway/train/images"
+ANN_DIR = "../Norway/train/annotations/xmls"
+JSON_DIR = "../NorwayJSON"
+COCO_DIR = "../NorwayCOCO"
 
 TRAINING_SET = "train_coco.json"
 VALIDATION_SET = "validation_coco.json"
