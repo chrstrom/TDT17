f0cf1c3ac7a5255c0fc8978328f00143e6d4dfd7
Christopher Strøm <detoxte@gmail.com>
2022-11-25 14:48:50 +0100

    Make bbox pixels integers
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- mini-project/test_model_to_rdd.py
+++ mini-project/test_model_to_rdd.py
@@ -3,14 +3,15 @@ import torch
 import glob
 
 import os
+ 
+from tqdm import tqdm
 
-
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5n - yolov5x6, custom
+model = torch.hub.load('ultralytics/yolov5', 'custom', path="best_model.pt",force_reload=True,autoshape=True)
 
 path = './Norway/test/images/*.jpg'
 
 predictions = []
-for filename in glob.glob(path): 
+for filename in tqdm(glob.glob(path)): 
 
     results = model(filename)
     image_id = os.path.basename(filename)
@@ -18,18 +19,17 @@ for filename in glob.glob(path):
 
     prediction_string = ""
     for object in bbox:
-        object_class = object[5] + 1 # RDD assumes 1-indexed classes
-        xmin = object[0]
-        xmax = object[2]
-        ymin = object[1]
-        ymax = object[3]
+        object_class = int(object[5] + 1) # RDD assumes 1-indexed classes
+        xmin = int(round(object[0], 0))
+        xmax = int(round(object[2], 0))
+        ymin = int(round(object[1], 0))
+        ymax = int(round(object[3], 0))
 
         prediction_string += f"{object_class} {xmin} {ymin} {xmax} {ymax} "
 
     submission_string = image_id + ", " + prediction_string 
 
     predictions.append(submission_string)
-
 
 
 with open("rdd_submissions.txt", "w") as file:
